4eaec73dddf09d94482dd7d9a35015f443209f82
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2024-02-12 22:20:32 +0530

    removing error

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="text-gray-600 body-font bg-slate-400 top-0 sticky z-20">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <span className="ml-3 text-xl">Developer's Portfolio</span>
+          <span className="ml-3 text-xl">Portfolio Website</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <Link href="/" className="mr-5 hover:text-gray-900">
